c29d40fb12f8c8caa358d84a4d9445c8b16a1c00
Nathan <Nathan@NATHAN-PC>
2021-11-18 20:57:57 -0500

    Added template character

--- main.py
+++ main.py
@@ -24,7 +24,7 @@ class Character:
     talent = 0
 
     defReduction = 0
-    resistReduction = .4
+    resistReduction = .0
 
     otherBonus = 1  # evilsoother
 
@@ -34,28 +34,60 @@ class Enemy:
     baseResist = .1
 
 def main():
+    
+    # Character Template #
+    template = Character()
+    template.AtkChar = 0
+    template.AtkWeapon = 0
+    template.AtkBonus = 0
+    template.FlatAtk = 0
+    template.DefChar = 0
+    template.DefBonus = 0
+    template.FlatDmg = 0
+    template.DmgBonus = 0
+    template.CR = 0
+    template.CD = 0
+    template.LvlChar = 0
+    template.EM = 0
+    template.ReactionBonus = 0
+    template.talent = 0
+    template.defReduction = 0
+    template.resistReduction = .0
+    template.otherBonus = 1
 
     ### Character
     childe = Character()
-
     childe.LvlChar = 90
     childe.AtkChar = 301
     childe.AtkWeapon = 674
     childe.AtkBonus = .48 + .20 + .25 + .466       # ttds + noblesse + pyro 48 + 20 + 25
     childe.FlatAtk = 311 #+ 1000          # bennett buff
-
     childe.DefChar = 815
     childe.DefBonus = 0
     childe.FlatDmg = 0
     childe.DmgBonus = .754
-
     childe.CR = 0.7
     childe.CD = 1.4
-
     childe.EM = 200
     childe.ReactionBonus = 0
-
     childe.talent = 6.0544 # Lvl 8 ranged burst
+    childe.resistReduction = .4
+
+    raiden = Character()
+    raiden.LvlChar = 90
+    raiden.AtkChar = 337
+    raiden.AtkWeapon = 674
+    raiden.AtkBonus = .2
+    raiden.FlatAtk = 311 + 1600
+    raiden.DefChar = 0
+    raiden.DefBonus = 0
+    raiden.FlatDmg = 0
+    raiden.DmgBonus = .32 + .2272*.25 + .466 + .3   # kazuha + emblem + goblet + raiden E
+    raiden.CR = .765
+    raiden.CD = 2.289
+    raiden.talent = 7.2144+(60*7*.01)
+    raiden.defReduction = .4
+    raiden.resistReduction = .4
 
     ### Enemy
 
@@ -65,10 +97,10 @@ def main():
 
     #calc(childe, e)
 
-    runCount = 100000
+    runCount = 100
     dmgArr = []
     for i in range(runCount):
-        d = calc(childe,e)
+        d = calc(raiden,e)
         dmgArr.append(d)
         print(d)
 
