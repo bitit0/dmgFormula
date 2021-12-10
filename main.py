f741af8707935776df3694557ab0f9d53d96c822
Nathan <Nathan@NATHAN-PC>
2021-12-10 10:49:23 -0500

    optimized output, added temporary redhorn, added DEF calculations

--- main.py
+++ main.py
@@ -34,6 +34,7 @@ class Character:
     resistReduction = .0    # Resist reduction (VV)
     otherBonus = 1          # 1.5 if Evilsoother is active
     ER = 0                  # ER
+    FlatDef = 0
 
     talent = 0              # talent value
 
@@ -54,6 +55,8 @@ class Character:
                 self.CD += item.mainStat[1]
             if item.mainStat[0] == "ER%":
                 self.ER += item.mainStat[1]
+            if item.mainStat[0] == "EM":
+                self.EM += item.mainStat[1]
             for subs in item.subStat:
                 if subs[0] == "ATK":
                     self.FlatAtk += subs[1]
@@ -67,11 +70,15 @@ class Character:
                     self.EM += subs[1]
                 if subs[0] == "ER%":
                     self.ER += subs[1]
+                if subs[0] == "DEF%":
+                    self.DefBonus += subs[1]
+                if subs[0] == "DEF":
+                    self.FlatDef += subs[1]
 
 
 class Enemy:
 
-    lvl = 93
+    lvl = 90
     baseResist = .1
 
 
@@ -121,48 +128,56 @@ def main():
     # goblet = Artifact(["Electro%", .466], ["DEF", 42], ["CR%", .089], ["CD%", .148], ["ER%", .058])
     # circlet = Artifact(["CD%", .622], ["ATK%", .175], ["CR%", .105], ["ER%", .052], ["DEF", 23])
 
-    flower = Artifact(["HP", 4780], ["ATK", 18], ["ATK%", .093], ["CD%", .218], ["EM", 54])
-    feather = Artifact(["ATK", 311], ["DEF", 16], ["CR%", .105], ["CD%", .148], ["HP", 837])
-    sands = Artifact(["ATK%", .466], ["DEF", 39], ["CR%", .101], ["CD%", .210], ["DEF%", .058])
-    goblet = Artifact(["Cryo%", .466], ["HP", 269], ["CR%", .074], ["CD%", .117], ["ER%", .155])
-    circlet = Artifact(["CD%", .622], ["ATK%", .140], ["CR%", .035], ["ER%", .058], ["HP%", .181])
+    # flower = Artifact(["HP", 4780], ["ATK", 18], ["ATK%", .093], ["CD%", .218], ["EM", 54])
+    # feather = Artifact(["ATK", 311], ["DEF", 16], ["CR%", .105], ["CD%", .148], ["HP", 837])
+    # sands = Artifact(["ATK%", .466], ["DEF", 39], ["CR%", .101], ["CD%", .210], ["DEF%", .058])
+    # goblet = Artifact(["Cryo%", .466], ["HP", 269], ["CR%", .074], ["CD%", .117], ["ER%", .155])
+    # circlet = Artifact(["CD%", .622], ["ATK%", .140], ["CR%", .035], ["ER%", .058], ["HP%", .181])
+
+    flower = Artifact(["HP", 4780], ["CR%", .101], ["ATK%", .058], ["CD%", .187], ["EM", 21])
+    feather = Artifact(["ATK", 311], ["DEF%", .066], ["EM", 37], ["CD%", .303], ["ATK%", .093])
+    sands = Artifact(["EM", 187], ["DEF", 23], ["ER%", .104], ["CD%", .311], ["DEF%", .073])
+    goblet = Artifact(["Pyro%", .466], ["HP%", .047], ["CR%", .07], ["CD%", .28], ["ER%", .058])
+    circlet = Artifact(["CR%", .622], ["ATK%", .093], ["CD%", .202], ["HP%", .111], ["EM", 19])
 
     artifactList = [flower, feather, sands, goblet, circlet]
 
     test = Character(artifactList)
     test.AtkChar = 335
-    test.FlatAtk += 0 # bennett and sara
-    test.DmgBonus += .15
-    test.resistReduction = .15
+    test.FlatAtk += 925 # bennett and sara
+    test.DmgBonus += .31 + .15    # serpent spine, kazuha, crimson
+    test.resistReduction = 0
     test.defReduction = 0
     test.LvlChar = 90
-    test.CD += .384   # Sara c6 Buff
+    test.CD += .882   # Sara c6 Buff
     test.CR += 0
-    test.AtkWeapon = 23        # jade spear
-    test.AtkBonus += 0
-    test.talent = 3.92
+    test.AtkWeapon = 542        # jade spear
+    test.AtkBonus += .45 + .18        # pyro res, noblesse, shimenawa
+    test.talent = 2.0608        # TL8 E
+    test.DefBonus += .28
+    test.DefChar = 784
     #test.FlatDmg = 2673.44
 
     e = Enemy()
     #talent = 7.2144+(60*7*.01)
 
-    runCount = 10000
-    dmgArr = []
-    for i in range(runCount):
-        d = calc(test,e, 1)
-        dmgArr.append(d)
-        #print(d)
+    # N1 E N2 E N3 E N4
+    combo = [153.32, 151.04, 149.79, 156.16, 168.9, 206.08, 229.03]
+    for i in range(len(combo)): combo[i] = combo[i]/100.0
 
-    print(f"\nAverage DMG over {runCount} runs: {sum(dmgArr)/runCount}")
+    print("Non crit: ", calc(test, e, 0))
+    print("On crit: ", calc(test, e, 1))
+    print("Average: ", (calc(test, e, 0) + calc(test, e, 1))/2.0)
+
 
 
 def calc(c: Character, e: Enemy, forceCrit: int):
 
     # general
 
-    # ampReaction
-    ampReaction = 2 * (1 + (2.78 * c.EM) / (1400 + c.EM) + c.ReactionBonus)
-    ampReaction = 1
+    # ampReaction (2: forward, 1.5: reverse)
+    ampReaction = 1.5 * (1 + (2.78 * c.EM) / (1400 + c.EM) + c.ReactionBonus)
+    #ampReaction = 1
 
     # enemyResMult
     resistance = e.baseResist - c.resistReduction
@@ -181,23 +196,27 @@ def calc(c: Character, e: Enemy, forceCrit: int):
     # Crit Formula
 
     if forceCrit == 0:
-        r = random.uniform(0, 1)
         crit = 1
-        if (r < c.CR):
-            crit = 1 + c.CD
     else:
         crit = 1 + c.CD
+
+    # Defense formula
+    defense = c.DefChar * (1 + c.DefBonus) + c.FlatDef
 
     # attack
     attack = (c.AtkChar + c.AtkWeapon) * (1 + c.AtkBonus) + c.FlatAtk
 
+    # temporary
+    c.FlatDmg = defense * .4
+
     # baseDamage
-    baseDamage = c.talent * attack + c.FlatDmg
+    baseDamage = c.talent * attack + c.FlatDmg      # attack scaling
+    #baseDamage = c.talent * defense + c.FlatDmg     # defense scaling
 
     damage = baseDamage * (
                 1 + c.DmgBonus) * crit * enemyDefMult * enemyResMult * ampReaction * c.otherBonus  # no transformative
 
-    print(ampReaction)
+    #print()
 
     return damage
 
